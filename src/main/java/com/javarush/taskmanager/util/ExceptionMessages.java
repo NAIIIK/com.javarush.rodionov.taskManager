b45c88297e99b15d38d8863ffbffeac0f558a0fd
RodionovNikita <radzivonaumikita@gmail.com>
2026-09-11 17:30:31 +0200

    refactor: add ExceptionMessages util class

---
+++ src/main/java/com/javarush/taskmanager/util/ExceptionMessages.java
@@ -0,0 +1,13 @@
+package com.javarush.taskmanager.util;
+
+public final class ExceptionMessages {
+
+    private ExceptionMessages() {}
+
+    public static final String NOT_A_MEMBER_MSG = "You are not a member of this project";
+    public static final String INVALID_CREDENTIALS_MSG = "Invalid credentials";
+
+    public static final String PROJECT_NOT_FOUND_MSG = "Project not found: ";
+    public static final String USER_NOT_FOUND_MSG = "User not found: ";
+    public static final String TASK_NOT_FOUND_MSG = "Task not found: ";
+}
